dcfad80473a9b7c21c2de672a8f7f517232ad4e9
Ahmed-Laaziz <101425997+Ahmed-Laaziz@users.noreply.github.com>
2023-08-24 15:18:14 +0200

    Update open_ai.py

--- open_ai.py
+++ open_ai.py
@@ -5,6 +5,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import openai
 
+from research_paper.Abstract import AbstractGenerator
+from research_paper.Title import TitleGenerator
+from research_paper.ChatBot import ChatBotAnswersGenerator
+from research_paper.ChatBotNlp import ChatBotAnswersGeneratorNLP
+from Translation import Translator
+from Summarization import TextSummarizer
+from Pydantic import AbstractRequest, TitleRequest, ChatBotAnswerRequest, TranslatorAnswerRequest, SummarizerAnswerRequest
+
 app = FastAPI()
 
 app.add_middleware(
@@ -33,6 +41,19 @@ def generate_title(field):
     )
     title =(response.choices[0].text).replace('\n', '').replace('\"', '')
     return title
+
+
+#TITLE
+def generate_summarize():
+    # Generate text using the GPT-3.5 model
+    prompt = f"can you summarize for me the research paper please ?"
+    response = openai.Completion.create(
+        engine="text-davinci-003",  # Specify the GPT-3.5 model
+        prompt=prompt,
+        max_tokens=150  # Adjust the desired length of generated text
+    )
+    res =(response.choices[0].text).replace('\n', '').replace('\"', '')
+    return res
 
 #ABSTRACT
 def generate_abstract(title):
@@ -97,18 +118,99 @@ def generate_research_paper(field : str):
     body = generate_body(title)
     conclusion = generate_conclusion(title)
     references = generate_references(title)
+    res = generate_summarize()
     response_data = {
         "title": title,
         "abstract": abstract,
         "introduction":introduction,
         "body":body,
         "conclusion":conclusion,
-        "references":references
+        "references":references,
+        "sum":res
     }
     return JSONResponse(content=response_data)
 
+@app.post("/generate_abstract")
+def generate_abstract_from_gpt2(request: AbstractRequest):
+    generator = AbstractGenerator()
+    #title = "Python as a powerful programming language for Data Science"
+    prompt = f"Q: Can you give me an abstract for my research paper with the Title:{request.title}?A: Abstract:" + request.abstract_text
+    abstract = generator.generate_abstract(request.title, prompt=prompt, max_length = request.words_to_generate)
+    print(prompt)
+    # Split the generated text based on "A: Abstract:"
+    split_text = abstract.split("A: Abstract:")
+    # Extract the text after "A: Abstract:"
+    if len(split_text) > 1:
+        abstract_text = split_text[1].strip()  # Remove leading/trailing whitespaces
+    else:
+        abstract_text = "No abstract found."
+    print(abstract_text)
+    return abstract_text
+
+@app.post("/generate_title")
+def generate_abstract_from_gpt2(request: TitleRequest):
+    generator = TitleGenerator()
+    title = generator.generate_title(field = request.field)
+    # Split the generated text based on "A: Abstract:"
+    print(title)
+    split_text = title.split("A: Title:")
+    # Extract the text after "A: Abstract:"
+    print(split_text)
+    if len(split_text) > 1:
+        title_text = (split_text[1].split("Q: Can you"))[0].strip()  # Remove leading/trailing whitespaces
+    else:
+        title_text = "No title found."
+    return title_text
+
+@app.post("/get_chatbot_answer")
+def generate_chatbot_answers_from_gpt2(request: ChatBotAnswerRequest):
+    generator = ChatBotAnswersGenerator()
+    chatAnswer = generator.generate_answer(question = request.question)
+    #Split the generated text based on "A: Abstract:"
+    split_text = chatAnswer.split("Bot: ")
+    # Extract the text after "?"
+    print(split_text)
+    if len(split_text) > 1:
+       #answer = split_text[1].strip()  # Remove leading/trailing whitespaces
+       answer = (split_text[1].split("User:"))[0].strip()
+    else:
+       answer = "No title found."
+    return answer
+
+@app.post("/get_chatbot_answer_nlp")
+def generate_chatbot_answers_from_gpt2(request: ChatBotAnswerRequest):
+    generator = ChatBotAnswersGeneratorNLP()
+    chatAnswer = generator.generate_answer(question = request.question)
+    #Split the generated text based on "A: Abstract:"
+    split_text = chatAnswer.split("Bot: ")
+    # Extract the text after "?"
+    print(split_text)
+    if len(split_text) > 1:
+       #answer = split_text[1].strip()  # Remove leading/trailing whitespaces
+       answer = (split_text[1].split("User:"))[0].strip()
+    else:
+       answer = "No title found."
+    return answer
+
+@app.post("/get_translation")
+def generate_translation(request: TranslatorAnswerRequest):
+    translator = Translator("facebook/nllb-200-distilled-600M", src_lang=request.src, tgt_lang=request.tgt)
+    return translator.translate_text(text = request.text)[0]['translation_text']
+
+@app.post("/get_summarization")
+def generate_summarization(request: SummarizerAnswerRequest):
+    summarizer = TextSummarizer()
+    return summarizer.summarize_text(request.text[:2048])[0]['summary_text']
+"""
+@app.get("/get_translation")
+def generate_translation(text: str, src:str, tgt:str):
+    translator = Translator("facebook/nllb-200-distilled-600M", src_lang=src, tgt_lang=tgt)
+    return translator.translate_text(text = text)[0]['translation_text']
+"""
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="localhost", port=8008)
+
 
 
