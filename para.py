8258095a70d1543bec45becd73d3bd18953d021a
unknown <fbarrade13@gmail.com>
2023-09-03 20:18:18 +0100

    Paraphrasing Integrated

--- para.py
+++ para.py
@@ -6,9 +6,34 @@ import uvicorn
 import os
 from parrot import Parrot 
 from uvicorn import Config, Server
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 #Init models
 with open("parrot_model.pkl", 'rb') as f:
     parrot= pickle.load(f)
+def create_paraphrase(full_text):
+    phrases = [x.strip() for x in full_text.split('.')]
+    print(phrases)
+    output_phrases = []
+    for phrase in phrases:
+        if (len(phrase) >1 ):
+            print("-"*100)
+            print("Input_phrase: ", phrase)
+            print("-"*100)
+            para_phrases = parrot.augment(input_phrase=phrase)
+            try:
+                for para_phrase in para_phrases:
+                    (x, y) = para_phrase
+                    print(x, y)
+                    x = x[0].upper() + x[1:] # captialise
+                    output_phrases.append(x)
+                    break # just get the first phrase
+            except:
+                print("Exception occured with this one.")
+                output_phrases.append(phrase)
+    
+    return ".".join(output_phrases)
 def paraphrase_paragraph(input_paragraph):
     phrases = sent_tokenize(input_paragraph)
 
@@ -26,21 +51,46 @@ def paraphrase_paragraph(input_paragraph):
       rejoined_paragraph+=result[i]+" "
     return(rejoined_paragraph)
 app = FastAPI()
+app = FastAPI()
+app.mount("/assets", StaticFiles(directory="front-end/assets"), name="assets")
+app.mount("/vendor", StaticFiles(directory="front-end/vendor"), name="vendor")
+
 @app.get("/")
-def test():
-  return "working"
-@app.post("/paraphrase")
+def read_index(request: Request):
+     return FileResponse("front-end/index.html")
+
+@app.get("/authentification")
+def read_generation():
+    return FileResponse("front-end/authentification.html")
+
+@app.get("/generation")
+def read_generation():
+    return FileResponse("front-end/generation.html")
+
+@app.get("/paraphrase")
+def read_paraphrase():
+    return FileResponse("front-end/paraphrase.html")
+@app.post("/paraphraser")
 def paraphrase(input_data: dict):
     input_paragraph = input_data.get("input_paragraph")
-    paraphrased_paragraph = paraphrase_paragraph(input_paragraph)
+    paraphrased_paragraph = create_paraphrase(input_paragraph)
+    print(paraphrased_paragraph)
     return {"paraphrased_paragraph": paraphrased_paragraph}
-
-
-
+@app.get("/recommendation")
+def read_recommendation():
+    return FileResponse("front-end/recommendation.html")
 if __name__ == "__main__":
-    config = Config(app=app, host="0.0.0.0", port=os.environ.get("PORT", 8000))
-    server = Server(config)
-    server.run()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+
+
+
+
+
+
+
 
 
   
